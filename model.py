c80dcc172f52fc19626d23bdf3ad3370b95d7be5
John Gay <gayj@Johns-MacBook-Pro.local>
2019-07-16 11:25:06 -0400

    encoding hotfix

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('training_set.csv') as file: 
+with open('training_set.csv', encoding="ISO 8859-1") as file: 
 	training_set = csv.DictReader(file)
 	for row in training_set:
 		print(row)
