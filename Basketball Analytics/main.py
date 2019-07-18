be91c78bd1752efb8a5fed023546d5c813000569
John Gay <gayj@Johns-MBP.home>
2019-07-18 18:26:02 -0400

    tests

--- Basketball Analytics/main.py
+++ Basketball Analytics/main.py
@@ -1,12 +1,14 @@
 import math
 import numpy as np
-import csv
 
 def parse_plays(playfile):
 	with open(playfile) as file:
-		reader = csv.DictReader(file)
-		for row in reader:
-			print(row)
+		i=0
+		for line in file:
+			print(line.split()[0].strip("\""))
+			i += 1
+			if i == 2:
+				break
 
 
 if __name__ == "__main__" :
