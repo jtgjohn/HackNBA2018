adb764bec13a099ebe8c264423533554eeb477e4
Owner <owner@Owners-MBP-2.home>
2019-07-18 19:57:25 -0400

    deleted extra

--- Business Analytics/model.py
+++ Business Analytics/model.py
@@ -14,7 +14,7 @@ df['Created'] = pd.to_datetime(df['Created'])
 #month variable
 df['Month'] = df['Created'].dt.month_name()
 month_dummy = pd.get_dummies(df['Month'], drop_first = True)
-print(month_dummy)
+
 #weekday variable
 df['Weekday'] = df['Created'].dt.day_name()
 Weekday_dummy = pd.get_dummies(df['Weekday'], drop_first = True)
@@ -31,17 +31,15 @@ df = df.drop(['Weekday'], axis = 1, inplace = False)
 #Dealing with categorical variable 'Type': dropping one dummy to prevent multicolinearity
 df = pd.get_dummies(df, drop_first = True) 
 df = df.join(Weekday_dummy)
-#print(df)
 
 #OLS (and probably better models in the future)
 Y = df.iloc[:, 0]
-X = df.iloc[:, [1,8]]
+X = df.iloc[:, 1:21]
 X = sm.add_constant(X)
 results = sm.OLS(Y, X).fit()
-#print(results.summary())
+print(results.summary())
 
 #cross validation 
-#test#
 
 
 
--- model.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('training_set.csv', encoding="ISO 8859-1") as file: 
-	training_set = csv.DictReader(file)
-	for row in training_set:
-		print(row)
-		break
-
